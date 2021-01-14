c4269855f9a840c10edcda3946c8a3492fd61b02
Akonya <akonya@adtel.co.ke>
2021-01-14 09:29:36 +0300

    feat: zuul cloud gateway proxy setup

---
+++ cloud-app1-gateway/src/test/java/com/aim/gateway/CloudApp1GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aim.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApp1GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
